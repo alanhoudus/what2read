f476c668e83a1cf02ad055cb800d40f28f63d4a9
Anissa-Malek <anissa.malek6@gmail.com>
2022-01-25 15:54:31 +0100

    buttons css ok

--- src/components/Profile/EditProfile/index.js
+++ src/components/Profile/EditProfile/index.js
@@ -9,17 +9,17 @@ const EditProfile = () => (
       <img alt="profileicon" src={profileicon} className="profile-logo" />
       <div className="profile-contentGroup">
         <h2 className="profile-content title">Mon profil</h2>
-        <h3 className="profile-content subtitle">Pseudo<button type="button" className="profile-content edit-name"><Edit /></button></h3>
+        <h3 className="profile-content subtitle">Pseudo<button type="button" className="profile-content-button edit-name"><Edit /></button></h3>
 
-        <img alt="profileicon" src={profileicon} className="profile-content avatar" /><button type="button" className="profile-content edit-avatar"><Upload /></button>
-        <div className="profile-content subtitle-description">Présentation<button type="button" className="profile-content edit-description"><Edit /></button>
+        <img alt="profileicon" src={profileicon} className="profile-content avatar" /><button type="button" className="profile-content-button edit-avatar"><Upload /></button>
+        <div className="profile-content subtitle">Présentation<button type="button" className="profile-content-button edit-description"><Edit /></button>
         </div>
         <p className="profile-content description">
           Lorem ipsum dolor sit amet, consectetur
           adipisicing elit. Saepe iurequibusdam suscipit unde, ullam ut aliquam accusamus
           dolor blanditiis officia. Fuga, porro odit expedita
         </p>
-        <p className="profile-content mail">mon.mail@gmail.com<button type="button" className="profile-content edit-mail"><Edit /></button></p>
+        <p className="profile-content mail">mon.mail@gmail.com<button type="button" className="profile-content-button edit-mail"><Edit /></button></p>
       </div>
     </div>
   </div>
