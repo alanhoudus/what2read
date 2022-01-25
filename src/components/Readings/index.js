89de506fa5d0ee663d6218ed615b49373119e714
Anissa-Malek <anissa.malek6@gmail.com>
2022-01-25 16:51:54 +0100

    starting integration with 3 books

--- src/components/Readings/index.js
+++ src/components/Readings/index.js
@@ -6,18 +6,29 @@ const Readings = () => (
     <h2 className="readings-title">Mes lectures</h2>
     <button type="button" className="readings-buttonRead">Ajouter un livre lu</button>
     <div className="readings-books">
-      <div className="reeading-book">
+      <div className="reading-book">
+        <a href="#">
+          <h3 className="readings-book--title">Hunger Games</h3>
+          <h4>Tome 1</h4>
+        </a>
         <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-1/9782266260770-475x500-1.webp" alt="title" />
-        <h3 className="readings-book title">Hunger Games</h3>
-        <h4>Tome 1</h4>
-        <p>Suzanne Collins</p>
       </div>
-      <div className="reeading-book">
-        <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-2-l-embrasement/9782266260787-475x500-1.webp" alt="title" />
-        <h3 className="readings-book title">Hunger Games</h3>
+      <div className="reading-book">
+        <a href="#">
+          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-2-l-embrasement/9782266260787-475x500-1.webp" alt="title" />
+        </a>
+        <h3 className="readings-book--title">Hunger Games</h3>
         <h4>Tome 2 - L'embrasement</h4>
       </div>
+      <div className="reading-book">
+        <a href="#">
+          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-3-la-revolte/9782266260794-475x500-1.webp" alt="title" />
+        </a>
+        <h3 className="readings-book--title">Hunger Games</h3>
+        <h4>Tome 3 - La révolte</h4>
+      </div>
     </div>
+    <button type="button" className="search-showMore">Voir plus</button>
   </div>
 );
 
