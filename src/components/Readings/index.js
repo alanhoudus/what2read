e67a137af734372119278cfb072cd5e39fdfad70
Anissa-Malek <anissa.malek6@gmail.com>
2022-01-25 17:23:06 +0100

    most of the mobile and desktop css are ok, still need some changes

--- src/components/Readings/index.js
+++ src/components/Readings/index.js
@@ -6,26 +6,26 @@ const Readings = () => (
     <h2 className="readings-title">Mes lectures</h2>
     <button type="button" className="readings-buttonRead">Ajouter un livre lu</button>
     <div className="readings-books">
-      <div className="reading-book">
+      <div className="readings-book">
         <a href="#">
           <h3 className="readings-book--title">Hunger Games</h3>
-          <h4>Tome 1</h4>
+          <h4 className="readings-book--subtitle">Tome 1</h4>
+          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-1/9782266260770-475x500-1.webp" alt="title" />
         </a>
-        <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-1/9782266260770-475x500-1.webp" alt="title" />
       </div>
-      <div className="reading-book">
+      <div className="readings-book">
         <a href="#">
+          <h3 className="readings-book--title">Hunger Games</h3>
+          <h4 className="readings-book--subtitle">Tome 2 - L'embrasement</h4>
           <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-2-l-embrasement/9782266260787-475x500-1.webp" alt="title" />
         </a>
-        <h3 className="readings-book--title">Hunger Games</h3>
-        <h4>Tome 2 - L'embrasement</h4>
       </div>
-      <div className="reading-book">
+      <div className="readings-book">
         <a href="#">
+          <h3 className="readings-book--title">Hunger Games</h3>
+          <h4 className="readings-book--subtitle">Tome 3 - La révolte</h4>
           <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-3-la-revolte/9782266260794-475x500-1.webp" alt="title" />
         </a>
-        <h3 className="readings-book--title">Hunger Games</h3>
-        <h4>Tome 3 - La révolte</h4>
       </div>
     </div>
     <button type="button" className="search-showMore">Voir plus</button>
