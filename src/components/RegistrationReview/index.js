7b6f92f59457bfb41d253768161c81b77d7cb18b
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-01-25 15:29:13 +0100

    component RegistrationReview css mobile done, some bug i have to fixe on the responsive desktop

--- src/components/RegistrationReview/index.js
+++ src/components/RegistrationReview/index.js
@@ -1,5 +1,7 @@
 // import components
 import Field from '../Field';
+// import asset
+import profilLogo from '../../assets/images/profileicon.png';
 // import scss
 import './registrationReview.scss';
 
@@ -7,6 +9,7 @@ const RegistrationReview = () => (
   <div className="addReview">
     <h2 className="addReview-title desktop">Formulaire d'ajout d'une review</h2>
     <h2 className="addReview-title mobile">Ecrire une review</h2>
+    <img src={profilLogo} alt="profile" className="addReview-logo" />
     <div className="addReview-group">
       <img src="https://products-images.di-static.com/image/john-ronald-reuel-tolkien-le-seigneur-des-anneaux-integrale/9782266286268-475x500-1.webp" alt="titleBook" className="addReview-cover" />
       <h3 className="addReview-titleBook">Le Seigneur des Anneaux</h3>
