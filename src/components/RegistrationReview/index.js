bcddfa309e02ab03f30685b3c2e4373cfaec841a
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-01-25 20:06:34 +0100

    component registrationReview done with css responsive mobile/desktop

--- src/components/RegistrationReview/index.js
+++ src/components/RegistrationReview/index.js
@@ -6,17 +6,20 @@ import profilLogo from '../../assets/images/profileicon.png';
 import './registrationReview.scss';
 
 const RegistrationReview = () => (
+
   <div className="addReview">
+    <img src={profilLogo} alt="profile" className="addReview-logo" />
     <h2 className="addReview-title desktop">Formulaire d'ajout d'une review</h2>
     <h2 className="addReview-title mobile">Ecrire une review</h2>
-    <img src={profilLogo} alt="profile" className="addReview-logo" />
-    <div className="addReview-group">
+    <form className="addReview-form">
       <img src="https://products-images.di-static.com/image/john-ronald-reuel-tolkien-le-seigneur-des-anneaux-integrale/9782266286268-475x500-1.webp" alt="titleBook" className="addReview-cover" />
-      <h3 className="addReview-titleBook">Le Seigneur des Anneaux</h3>
-    </div>
-    <Field type="text" placeholder="Mets un titre à ta review" className="addReview-field title" />
-    <Field type="text" placeholder="Texte de la review" className="addReview-field content" />
-    <button type="submit" className="addReview-submit">Envoyer</button>
+      <Field id="title" type="text" label="Le Seigneur des Anneaux" placeholder="Mets un titre à ta review" className="addReview-field title" />
+      <div className="addReview-jumbotron">
+        <img src="https://products-images.di-static.com/image/john-ronald-reuel-tolkien-le-seigneur-des-anneaux-integrale/9782266286268-475x500-1.webp" alt="titleBook" className="addReview-cover" />
+        <textarea cols="30" rows="10" placeholder="Texte de la review" className="addReview-field content" />
+      </div>
+      <button type="submit" className="addReview-submit">Envoyer</button>
+    </form>
   </div>
 );
 
