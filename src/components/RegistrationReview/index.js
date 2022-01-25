4efaabd9228cc6d54459328cdc5e47e5a3f73bb9
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-01-25 13:41:50 +0100

    component RegistrationReview create working on it

--- src/components/RegistrationReview/index.js
+++ src/components/RegistrationReview/index.js
@@ -7,9 +7,9 @@ const RegistrationReview = () => (
   <div className="addReview">
     <h2 className="addReview-title desktop">Formulaire d'ajout d'une review</h2>
     <h2 className="addReview-title mobile">Ecrire une review</h2>
-    <div>
-      <h3 className="addReview-titleBook">Le Seigneur des Anneaux</h3>
+    <div className="addReview-group">
       <img src="https://products-images.di-static.com/image/john-ronald-reuel-tolkien-le-seigneur-des-anneaux-integrale/9782266286268-475x500-1.webp" alt="titleBook" className="addReview-cover" />
+      <h3 className="addReview-titleBook">Le Seigneur des Anneaux</h3>
     </div>
     <Field type="text" placeholder="Mets un titre à ta review" className="addReview-field title" />
     <Field type="text" placeholder="Texte de la review" className="addReview-field content" />
