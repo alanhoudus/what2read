a200d3f53bd88bd78ab4abdebe153f26313e4650
Anissa-Malek <anissa.malek6@gmail.com>
2022-01-26 17:06:27 +0100

    integration finished

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -9,15 +9,38 @@ const Suggestions = () => (
     <SuggestedBook />
     <div className="suggestions-separator" />
     <div className="suggestions-history">
-      <Link
-        to="/livre"
-        key="1"
-      >
-        <h3 className="suggestions-history book-title">Lire J. R. R. Tolkien</h3>
-        <h4 className="suggestions-history book-title">10.02.2022</h4>
-        <img className="suggestions-history-cover" src="https://products-images.di-static.com/image/vincent-ferre-lire-j/9782266242912-475x500-1.webp" alt="couverture du livre" />
-      </Link>
+      <div className="suggestions-history book">
+        <Link
+          to="/livre"
+          key="1"
+        >
+          <img className="suggestions-history book-cover" src="https://products-images.di-static.com/image/vincent-ferre-lire-j/9782266242912-475x500-1.webp" alt="couverture du livre" />
+          <h3 className="suggestions-history book-title">Lire J. R. R. Tolkien</h3>
+          <h4 className="suggestions-history book-title">10.02.2022</h4>
+        </Link>
+      </div>
+      <div className="suggestions-history book">
+        <Link
+          to="/livre"
+          key="1"
+        >
+          <img className="suggestions-history book-cover" src="https://products-images.di-static.com/image/vincent-ferre-lire-j/9782266242912-475x500-1.webp" alt="couverture du livre" />
+          <h3 className="suggestions-history book-title">Lire J. R. R. Tolkien</h3>
+          <h4 className="suggestions-history book-title">10.02.2022</h4>
+        </Link>
+      </div>
+      <div className="suggestions-history book">
+        <Link
+          to="/livre"
+          key="1"
+        >
+          <img className="suggestions-history book-cover" src="https://products-images.di-static.com/image/vincent-ferre-lire-j/9782266242912-475x500-1.webp" alt="couverture du livre" />
+          <h3 className="suggestions-history book-title">Lire J. R. R. Tolkien</h3>
+          <h4 className="suggestions-history book-title">10.02.2022</h4>
+        </Link>
+      </div>
     </div>
+    <button type="button" className="suggestions-showMore">Voir plus</button>
   </div>
 );
 
