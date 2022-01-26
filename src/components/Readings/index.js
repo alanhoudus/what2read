450fb984733220ce5f731f000e4a8f824b670009
Anissa-Malek <anissa.malek6@gmail.com>
2022-01-26 08:54:04 +0100

    added the other icons but no correct css done yet

--- src/components/Readings/index.js
+++ src/components/Readings/index.js
@@ -1,7 +1,7 @@
 // import scss
 import './readings.scss';
 import { Star } from 'react-feather';
-import FileText from 'react-feather';
+import { FileText } from 'react-feather';
 
 const Readings = () => (
   <div className="readings">
@@ -12,14 +12,14 @@ const Readings = () => (
         <a href="#">
           <h3 className="readings-book--title">Hunger Games</h3>
           <h4 className="readings-book--subtitle">Tome 1</h4>
-          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-1/9782266260770-475x500-1.webp" alt="title" /><Star className="readings favorited" color="#ffff00" />
+          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-1/9782266260770-475x500-1.webp" alt="title" /><Star className="readings favorited" color="#ffff00" /> <FileText className="readings review-done" color="#32CD32" />
         </a>
       </div>
       <div className="readings-book">
         <a href="#">
           <h3 className="readings-book--title">Hunger Games</h3>
           <h4 className="readings-book--subtitle">Tome 2 - L'embrasement</h4>
-          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-2-l-embrasement/9782266260787-475x500-1.webp" alt="title" />
+          <img className="search-img" src="https://products-images.di-static.com/image/suzanne-collins-hunger-games-tome-2-l-embrasement/9782266260787-475x500-1.webp" alt="title" /><Star className="readings not-favorited" color="#000" /> <FileText className="readings review-undone" color="#ff0000" />
         </a>
       </div>
       <div className="readings-book">
