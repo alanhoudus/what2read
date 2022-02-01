29e5ebd5edec9a57e439a5d71278f2912c56fb59
alanhoudus <alan.houdus@outlook.fr>
2022-02-01 12:17:11 +0100

    few fixes, removed an action

--- src/actions/books.js
+++ src/actions/books.js
@@ -1,17 +1,6 @@
 export const GET_BOOKS_DATA = 'GET_BOOKS_DATA';
 export const SAVE_BOOKS_LIST = 'SAVE_BOOKS_LIST';
 export const BOOKS_LIST_LOADED = 'BOOKS_LIST_LOADED';
-export const GET_BOOK_DATA = 'GET_BOOK_DATA';
-
-/**
- * Get the corresponding book data
- * @param {string} isbn
- * @returns
- */
-export const getBookData = (isbn) => ({
-  type: GET_BOOK_DATA,
-  data: isbn,
-});
 
 export const booksListLoaded = () => ({
   type: BOOKS_LIST_LOADED,
--- src/components/Search/SearchItem.js
+++ src/components/Search/SearchItem.js
@@ -1,35 +1,22 @@
-// hooks
-import { useDispatch } from 'react-redux';
-
 // react-router-dom
 import { Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 
-// actions
-import { getBookData } from '../../actions/books';
-
-const SearchItem = ({ searchedBook }) => {
-  const dispatch = useDispatch();
-
-  return (
-    <div
-      className="search-book"
-      onClick={(() => {
-        dispatch(getBookData(searchedBook.isbn));
-      })}
+const SearchItem = ({ searchedBook }) => (
+  <div
+    className="search-book"
+  >
+    <Link
+      to={`/livre/${searchedBook.isbn}`}
+      key={searchedBook.isbn}
     >
-      <Link
-        to={`/livre/${searchedBook.isbn}`}
-        key={searchedBook.isbn}
-      >
-        <img className="search-book cover" src={searchedBook.cover} alt="title" />
-        <h3 className="search-book--title">{searchedBook.title}</h3>
-        <h3 className="search-book--subtitle">{searchedBook.subtitle}</h3>
-      </Link>
-    </div>
-  );
-};
+      <img className="search-book cover" src={searchedBook.cover} alt="title" />
+      <h3 className="search-book--title">{searchedBook.title}</h3>
+      <h3 className="search-book--subtitle">{searchedBook.subtitle}</h3>
+    </Link>
+  </div>
+);
 
 SearchItem.propTypes = {
   searchedBook: PropTypes.shape({
