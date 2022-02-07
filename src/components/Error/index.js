bccddb54d4c49210ccd07d79dd62934ec81e8515
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-02-07 09:58:09 +0100

    dynamisation 404page not done yet

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -3,10 +3,21 @@ import './notfound.scss';
 
 // == Composant
 const NotFound = () => (
-  <div className="notfound">
-    <h2 className="notfound-title">Erreur 404</h2>
-    <p>La page que vous avez demandé n'existe pas</p>
+  <div className="bookAnimation">
+    <h1 className="book-title">Page not found</h1>
+    <div className="back" />
+    <div className="page6" />
+    <div className="page5" />
+    <div className="page4" />
+    <div className="page3" />
+    <div className="page2" />
+    <div className="page1" />
+    <div className="front" />
   </div>
+  // <div className="notfound">
+  //   <h2 className="notfound-title">Erreur 404</h2>
+  //   <p>La page que vous avez demandé n'existe pas</p>
+  // </div>
 );
 
 // == Export
