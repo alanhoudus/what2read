88399e09c4c9aae23452a39c0b5d1fc4455565d6
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-02-08 11:22:12 +0100

    handle error on registration if the mail or nickname already exist

--- src/actions/user.js
+++ src/actions/user.js
@@ -34,6 +34,11 @@ export const DELETE_FAVORIS_USER = 'DELETE_FAVORIS_USER';
 export const ADD_BOOK_TO_READINGS = 'ADD_BOOK_TO_READINGS';
 export const REMOVE_BOOK_FROM_READINGS = 'REMOVE_BOOK_FROM_READINGS';
 export const DELETE_USER_ACCOUNT = 'DELETE_USER_ACCOUNT';
+export const HANDLE_USER_ALREADY_EXIST = 'HANDLE_USER_ALREADY_EXIST';
+
+export const handleUserAlreadyExist = () => ({
+  type: HANDLE_USER_ALREADY_EXIST,
+});
 
 export const deleteUserAccount = () => ({
   type: DELETE_USER_ACCOUNT,
