91fe2c2af87c06f0a41953c093e744835a21cf1d
VirginieGaffinet <gaffinet.virginie@gmail.com>
2022-02-09 15:25:44 +0100

    contact page integration done with css

---
+++ src/data/dev.js
@@ -0,0 +1,34 @@
+const contact = [
+  {
+    name: 'Anissa',
+    avatar: 'https://thumbs.gfycat.com/UnfinishedWarlikeArmadillo-max-1mb.gif',
+    role: 'Scrum Master',
+    team: 'Front',
+  },
+  {
+    name: 'Virginie',
+    avatar: 'https://tenor.com/view/nom-tik-tok-kitty-cat-cat-biting-gif-18162240.gif',
+    role: 'Git Master',
+    team: 'Front',
+  },
+  {
+    name: 'Alan',
+    avatar: 'https://tenor.com/view/cat-dancing-led-light-rainbow-gif-17974316.gif',
+    role: 'Lead Front',
+    team: 'Front',
+  },
+  {
+    name: 'Olivier',
+    avatar: 'https://media.giphy.com/media/Lq0h93752f6J9tijrh/giphy.gif',
+    role: 'Lead Back',
+    team: 'Back',
+  },
+  {
+    name: 'Nolwenn',
+    avatar: 'https://tenor.com/view/distorted-cat-meme-distortion-cat-black-cat-meme-gif-14670421.gif',
+    role: 'Product Owner',
+    team: 'Back',
+  },
+];
+
+export default contact;
